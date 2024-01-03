ef4f2e28feb492322be5975b1b251ca7a5fb5a00
wangbing <wangbing@tripwise.cn>
2024-01-03 18:05:31 +0800

    feat: 自动注册element-plus icons组件

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
-import {createPinia} from "pinia";
+import {createPinia} from "pinia"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './assets/styles/variable.css'
 import './assets/styles/custom.css'
 import './assets/styles/font.css'
@@ -13,3 +14,6 @@ app.use(router)
 app.use(ElementPlus)
 app.use(pinia)
 app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
