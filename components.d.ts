637b330694b0de52982f8c95a91511608f7349eb
wangbing <wangbing@tripwise.cn>
2024-01-03 21:35:19 +0800

    feat: 美化滚动条

--- components.d.ts
+++ components.d.ts
@@ -6,17 +6,17 @@
 export {}
 
 declare module 'vue' {
-	export interface GlobalComponents {
-		ElButton: (typeof import('element-plus/es'))['ElButton']
-		ElDatePicker: (typeof import('element-plus/es'))['ElDatePicker']
-		ElForm: (typeof import('element-plus/es'))['ElForm']
-		ElFormItem: (typeof import('element-plus/es'))['ElFormItem']
-		ElInput: (typeof import('element-plus/es'))['ElInput']
-		ElOption: (typeof import('element-plus/es'))['ElOption']
-		ElSelect: (typeof import('element-plus/es'))['ElSelect']
-		ElTabPane: (typeof import('element-plus/es'))['ElTabPane']
-		ElTabs: (typeof import('element-plus/es'))['ElTabs']
-		RouterLink: (typeof import('vue-router'))['RouterLink']
-		RouterView: (typeof import('vue-router'))['RouterView']
-	}
+  export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElOption: typeof import('element-plus/es')['ElOption']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
 }
