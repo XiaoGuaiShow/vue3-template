d6869e2944fb6d72fe1b519310604c17b25d670e
wangbing <wangbing@tripwise.cn>
2024-01-03 21:38:04 +0800

    feat: 增加页面切换过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-	<router-view />
+  <router-view v-slot="{ Component, route }">
+    <transition name="animation" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </transition>
+  </router-view>
 </template>
 
 <style lang="less">
@@ -10,6 +14,23 @@ body,
 	height: 100%;
 	margin: 0;
 	padding: 0;
+
+  /* 过度动画配置代码 */
+  .animation-enter-from,
+  .animation-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+  .animation-enter-to,
+  .animation-leave-from {
+    opacity: 1;
+  }
+  .animation-enter-active {
+    transition: all 0.7s ease;
+  }
+  .animation-leave-active {
+    transition: all 0.3s cubic-bezier(1, 0.6, 0.6, 1);
+  }
 
 	// 滚动条整体部分
 	&::-webkit-scrollbar {
