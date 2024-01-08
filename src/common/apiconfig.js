6dcb5cf439b13e72f582625f27eb5ea6993df094
吕东键 <feeling@tripwise.cn>
2024-01-08 16:45:58 +0800

    feat: 接口请求配置

--- src/common/apiconfig.js
+++ src/common/apiconfig.js
@@ -1,56 +1,56 @@
 export default [
-  {
-    enterpriseId: 26720, //迈为
-    apiUrl: 'https://maiweiapi.ceekee.com/',
-    webUrl: 'http://maiwei.tripwise.cn/',
-    plantId: 1028,
-    mwebUrl: 'https://newmaiwei.ceekee.com/',
-    cropId: 'ding4fd7fb7f38562533',
-  },
-  {
-    enterpriseId: 41225, //明喆
-    apiUrl: 'https://mingzhewuyeapi.ceekee.com/',
-    webUrl: 'http://mingzhewuye.tripwise.cn/',
-    plantId: 1028,
-    mwebUrl: 'https://mingzhewuye.ceekee.com/',
-    cropId: 'ding1f63da81964e815f',
-  },
-  {
-    enterpriseId: 41327, //海平面
-    apiUrl: 'https://haipingmianapi.ceekee.com/',
-    webUrl: 'http://haipingmian.tripwise.cn/',
-    plantId: 1028,
-    mwebUrl: 'https://haipingmian.ceekee.com/',
-    cropId: 'ding44f7998aa828f2a535c2f4657eb6378f',
-  },
-  {
-    //汇联易渠道
-    enterpriseId: 0,
-    apiUrl: 'https://api.ceekeeapi.com/',
-    webUrl: '',
-    mwebUrl: 'https://distribution.ceekee.com/',
-    plantId: 1035,
-    refId: 154,
-    cropId: '',
-  },
-  {
-    //浩天渠道
-    enterpriseId: 0,
-    apiUrl: 'https://api.ceekeeapi.com/',
-    webUrl: '',
-    mwebUrl: 'https://distribution.ceekee.com/',
-    plantId: 1035,
-    refId: 152,
-    cropId: '',
-  },
-  {
-    //钉钉第三方配置
-    enterpriseId: 0,
-    apiUrl: 'https://api.ceekeeapi.com/',
-    webUrl: '',
-    mwebUrl: 'https://m.ceekee.com/',
-    plantId: 1028,
-    cropId: '',
-    type: 'thirdService',
-  },
-];
+	{
+		enterpriseId: 26720, //迈为
+		apiUrl: 'https://maiweiapi.ceekee.com/',
+		webUrl: 'http://maiwei.tripwise.cn/',
+		plantId: 1028,
+		mwebUrl: 'https://newmaiwei.ceekee.com/',
+		cropId: 'ding4fd7fb7f38562533'
+	},
+	{
+		enterpriseId: 41225, //明喆
+		apiUrl: 'https://mingzhewuyeapi.ceekee.com/',
+		webUrl: 'http://mingzhewuye.tripwise.cn/',
+		plantId: 1028,
+		mwebUrl: 'https://mingzhewuye.ceekee.com/',
+		cropId: 'ding1f63da81964e815f'
+	},
+	{
+		enterpriseId: 41327, //海平面
+		apiUrl: 'https://haipingmianapi.ceekee.com/',
+		webUrl: 'http://haipingmian.tripwise.cn/',
+		plantId: 1028,
+		mwebUrl: 'https://haipingmian.ceekee.com/',
+		cropId: 'ding44f7998aa828f2a535c2f4657eb6378f'
+	},
+	{
+		//汇联易渠道
+		enterpriseId: 0,
+		apiUrl: 'https://api.ceekeeapi.com/',
+		webUrl: '',
+		mwebUrl: 'https://distribution.ceekee.com/',
+		plantId: 1035,
+		refId: 154,
+		cropId: ''
+	},
+	{
+		//浩天渠道
+		enterpriseId: 0,
+		apiUrl: 'https://api.ceekeeapi.com/',
+		webUrl: '',
+		mwebUrl: 'https://distribution.ceekee.com/',
+		plantId: 1035,
+		refId: 152,
+		cropId: ''
+	},
+	{
+		//钉钉第三方配置
+		enterpriseId: 0,
+		apiUrl: 'https://api.ceekeeapi.com/',
+		webUrl: '',
+		mwebUrl: 'https://m.ceekee.com/',
+		plantId: 1028,
+		cropId: '',
+		type: 'thirdService'
+	}
+]
--- src/common/downloadExcel.js
+++ src/common/downloadExcel.js
@@ -1,29 +1,29 @@
 export default (res) => {
-  try {
-    // 获取Content-Disposition标头
-    const contentDisposition = res.headers['content-disposition'];
+	try {
+		// 获取Content-Disposition标头
+		const contentDisposition = res.headers['content-disposition']
 
-    // 从Content-Disposition标头中提取文件名
-    const fileNameMatch = /filename=([^;]+)/.exec(contentDisposition);
-    const fileName = fileNameMatch ? fileNameMatch[1] : 'excel_file.xlsx';
+		// 从Content-Disposition标头中提取文件名
+		const fileNameMatch = /filename=([^;]+)/.exec(contentDisposition)
+		const fileName = fileNameMatch ? fileNameMatch[1] : 'excel_file.xlsx'
 
-    // 创建一个Blob对象来存储Excel数据流
-    const blob = new Blob([res.data], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    });
+		// 创建一个Blob对象来存储Excel数据流
+		const blob = new Blob([res.data], {
+			type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+		})
 
-    // 创建一个下载链接
-    const url = window.URL.createObjectURL(blob);
+		// 创建一个下载链接
+		const url = window.URL.createObjectURL(blob)
 
-    // 创建一个<a>标签并设置其属性，然后模拟点击以触发下载
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = decodeURIComponent(fileName);
-    link.click();
+		// 创建一个<a>标签并设置其属性，然后模拟点击以触发下载
+		const link = document.createElement('a')
+		link.href = url
+		link.download = decodeURIComponent(fileName)
+		link.click()
 
-    // 释放Blob对象和URL对象
-    window.URL.revokeObjectURL(url);
-  } catch (error) {
-    console.error('下载Excel文件时出错：', error);
-  }
-};
+		// 释放Blob对象和URL对象
+		window.URL.revokeObjectURL(url)
+	} catch (error) {
+		console.error('下载Excel文件时出错：', error)
+	}
+}
