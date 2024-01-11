1ccf168ad6eec845fd3d587639e739fbb7884b4e
秦闯 <qinchuang@tripwise.cn>
2024-01-11 11:28:12 +0800

    feat: 引入字母接口

--- src/api/modules/parentCompany.ts
+++ src/api/modules/parentCompany.ts
@@ -21,8 +21,32 @@ export const accountAllList = (data: Data) => {
 // 公司详情
 export const accountDetail = (data: Data) => {
   return axiosInstance.request<RequestConfig>({
-    params:data,
+    params: data,
     method: 'get',
     url: 'platform/account/detail'
+  })
+}
+// 启用&停用 子公司
+export const accountValid = (data: Data) => {
+  return axiosInstance.request<RequestConfig>({
+    data,
+    method: 'post',
+    url: 'platform/account/valid'
+  })
+}
+// 扣款规则设置
+export const commonSetting = (data: Data) => {
+  return axiosInstance.request<RequestConfig>({
+    data,
+    method: 'post',
+    url: 'platform/account/common/setting'
+  })
+}
+// 扣款规则展示
+export const commonSettingInfo = (data: Data) => {
+  return axiosInstance.request<RequestConfig>({
+    params: data,
+    method: 'get',
+    url: 'platform/account/common/settingInfo'
   })
 }
